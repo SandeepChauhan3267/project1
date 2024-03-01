b8740646bcf119892921bd2cb5c4e46730716bb3
Sandeep Chauhan <sksandy05@gmail.com>
2024-03-02 03:45:27 +0530

    add backend

--- 2storageacc/provider.tf
+++ 2storageacc/provider.tf
@@ -5,6 +5,7 @@ terraform {
       version = "3.89.0"
     }
   }
+  
 }
 
 provider "azurerm" {
--- 4subnet/provider.tf
+++ 4subnet/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "3.90.0"
     }
   }
+     backend "azurerm" {
+    resource_group_name  = "chauhan-rg2"
+    storage_account_name = "chauhanstorageaccount2"
+    container_name       = "newcontainer"
+    key                  = "prod.terraform.subnet"
+  }
 }
 
 provider "azurerm" {
